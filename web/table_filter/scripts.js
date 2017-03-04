71708915c51e33778518d4bf751e4f208821dda2
Lenny Duan <dhy939694@gmail.com>
2017-03-04 13:19:33 +0000

    version 1 complete

--- web/table_filter/scripts.js
+++ web/table_filter/scripts.js
@@ -1,19 +1,28 @@
 function filterFunction() {
   // Declare variables
   var columnInput, filterInput, filter, table, tr, td, i, columnValue, head, headIndex;
+  // Get two input value
   columInput = document.getElementById("headerInput");
   filterInput = document.getElementById("filterInput");
+  columnValue = columInput.value.toUpperCase();
   filter = filterInput.value.toUpperCase();
+
+  // Define table and tr
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
 
-
-  columnValue = columInput.value.toUpperCase();
+  // Find Table Head Index
   if(columnValue) {
-  head = table.getElementsByTagName("th");
-} else {
-  headIndex = 0;
-}
+    head = table.getElementsByTagName("tr")[0].getElementsByTagName("th");
+    for (var j = 0; j < head.length; j++) {
+      if(head[j].innerHTML.toUpperCase() == columnValue) {
+        headIndex = j;
+        break;
+      }
+    }
+  } else {
+    headIndex = 0;
+  }
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
