5ff6763cb50ad93e27f66713814e85a4dc2bb56f
LennyDuan <hod8@aber.ac.uk>
2018-03-23 18:21:38 +0000

    happy pass

--- script/file/app/app.py
+++ script/file/app/app.py
@@ -1,14 +1,13 @@
 import os
-from objects import Result
-from objects import CourseMapper
-
+from objects import Result, Course, CourseMapper
+## Initializing
+print('###### Start Initializing ######')
 dir = os.getcwd()
 path = dir + '/map/mapper.csv'
 print('Find Course Mapper: ' + path)
-print('------------------')
 
 mapper = {}
-print('------ Initial Mapper ------')
+print('###### Initial Mapper ######')
 with open(path) as file:
     for line in file:
         line = line.strip()
@@ -16,7 +15,46 @@ with open(path) as file:
         result = CourseMapper(arr[0], arr[1], arr[2], arr[3], arr[4])
         mapper[arr[0]] = result
 
-print('------ Start Reading Mapper ------\n ')
+print('###### Start Reading Mapper ######\n ')
 for key, val in mapper.items():
     val.toString()
-print('------ End Reading Mapper ------')
+print('###### End Reading Mapper ######')
+print('###### End Initializing ######\n\n')
+
+## Start Converting
+courses = {}
+print('------------------------')
+name = input('Please input a full file name: \n')
+print('Your input file is: [' + name + ']');
+path = dir + '/results/' + name;
+print('###### Start Converting Scores from File... ###### \n')
+with open(path) as file:
+    for line in file:
+        line = line.strip()
+        eles = line.split(',')
+        result = Result(eles[0], eles[1], eles[2])
+        ## Looking in the mapper
+        resultId = result.id
+        if resultId in mapper:
+            courseName = mapper[resultId].cName
+            coursePoint = mapper[resultId].point
+            course = None
+
+            if courseName in courses:
+                course = courses[courseName]
+            else:
+                course = Course(courseName)
+                courses[courseName] = course
+
+            course.add(result, coursePoint)
+            print('Converting result: [' + result.id + ' ' + result.name
+            + '] to Course: [' + course.cName + '] with [' + coursePoint + '] point\n')
+        else:
+            print('Mapper cannot find the course: ')
+            result.toString()
+print('###### End Converting ######\n\n')
+print('------------------------\n\n')
+
+for key, val in courses.items():
+    val.toString()
+    print('------\n')
--- script/file/app/objects.py
+++ script/file/app/objects.py
@@ -10,14 +10,32 @@ class Result:
 
 class CourseMapper:
 
-    def __init__(self, id, eName, cName, min, score):
+    def __init__(self, id, eName, cName, min, point):
         self.id = id
         self.eName = eName
         self.cName = cName
         self.min = min
-        self.score = score
+        self.point = point
 
     def toString(self):
         print('Course: ' + self.id + ' ' + self.eName
          + '\n课程: ' + self.cName + '  最低转分要求: '
-         + '\n可转学分: ' + self.score + '\n')
+         + '\n可转学分: ' + self.point + '\n')
+
+class Course:
+
+    def __init__(self, cName):
+        self.cName = cName
+        self.results = []
+        self.sum = 0
+
+    def add(self, result, point):
+        self.results.append(result)
+        self.sum += int(point)
+
+    def toString(self):
+        print('课程: ' + self.cName)
+        print('转分来自: ')
+        for ele in self.results:
+            ele.toString()
+        print('总转学分: ' + str(self.sum))
