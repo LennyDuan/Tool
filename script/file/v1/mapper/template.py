5f5679c3074632fe33cbf6a3983e07cf6f5ba1ba
LennyDuan <hod8@aber.ac.uk>
2018-04-08 17:45:47 +0100

    init category

--- script/file/v1/mapper/template.py
+++ script/file/v1/mapper/template.py
@@ -4,10 +4,15 @@ from objects import DegreeCourse
 def createDegreeCourseTemplate(template, path):
     print('###### Initial Template ######\n')
     template = csv.reader(open(path))
+    category = None
     for arr in template:
-        createTemplate(template, arr)
+        createTemplate(template, arr, category)
     print('###### End Initializing ######\n')
     return template;
 
-def createTemplate(template, arr):
-    print(arr)
+def createTemplate(template, arr, category):
+    if (len(arr) == 1):
+        category = arr[0]
+        print('Current Category is: ' + category)
+    else:
+        print(arr)
