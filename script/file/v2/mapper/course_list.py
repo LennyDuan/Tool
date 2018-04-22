d462f40fd4e8e0a4c338b48ccd3aa92539f3cdd2
LennyDuan <hod8@aber.ac.uk>
2018-04-22 14:29:47 +0100

    finish courselist mapper

--- script/file/v2/mapper/course_list.py
+++ script/file/v2/mapper/course_list.py
@@ -2,12 +2,10 @@ import csv
 from objects import CourseList
 
 def createCourseList(course_list, path):
-    #print('\n###### Initial Category Template ######\n')
     file = csv.reader(open(path))
     for arr in file:
         createList(course_list, arr)
-    #print('\n###### End Category Initializing ######\n')
-    return category;
+    return course_list;
 
 def createList(course_list, arr):
-    category[arr[0]] = DegreeCategory(arr[0], arr[1])
+    course_list[arr[0]] = CourseList(arr[0], arr[2], arr[3], arr[7])
