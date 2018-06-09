60f416a463e0a150574749a796f074da07aadbbc
LennyDuan <hod8@aber.ac.uk>
2018-06-09 21:19:19 +0100

    init query 1 item

--- aws/lambda/index.js
+++ aws/lambda/index.js
@@ -20,3 +20,29 @@ const list = async (payload, context, callback) => {
     callback(error, 'Unable to scan this table')
   }
 };
+
+// get one object in a table
+export
+const list = async (payload, context, callback) => {
+  console.log(JSON.stringify(payload));
+  const event_security = payload.queryStringParameters.security;
+  const event_table_name = payload.queryStringParameters.table_name;
+  const uid = payload.queryStringParameters.uid;
+  const queryParameters = {
+    KeyConditionExpression: '#hkn = :qv',
+        ExpressionAttributeNames: {
+        "#hkn": "uid",
+        },
+        ExpressionAttributeValues: {
+        ":qv": uid,
+        },
+    TableName: event_table_name,
+  }
+  try {
+    const data = await docClient.getItem(queryParameters).promise();
+    callback(null, data)
+  } catch(error) {
+    console.log(error);
+    callback(error, 'Unable to query this item')
+  }
+};
