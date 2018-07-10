2d5069aafcc3b4b7b09818b38576fe46d514caa8
LennyDuan <hod8@aber.ac.uk>
2018-07-10 09:52:47 +0100

    PassPass

---
+++ script/passpass/pass/objects.py
@@ -0,0 +1,15 @@
+class PassPass
+
+    def __init__(self, name, account, password, extra):
+        self.name = name
+        self.account = account
+        self.password = password
+        if extra:
+            self.extra = extra
+        else:
+            self.extra = None
+
+    def toString(self):
+        print('Name: ' + self.name + ' Account: ' + self.account
+        + ' Password: ' + self.password + ' Extra: '
+        + self.extra
